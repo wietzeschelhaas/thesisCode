832cbbf47aa801be5beb8d13d9d9bc8271515296
wietzeschelhaas <wietze.pc@gmail.com>
2020-05-02 14:29:55 +0200

    preprocess

--- lstm.py
+++ lstm.py
@@ -6,45 +6,111 @@ Created on Wed Apr 22 09:30:37 2020
 """
 
 import pandas
-import tensorflow as tf
 from keras.models import Sequential
-from keras.layers import Dense,Dropout,LSTM
-from keras.optimizers import adam
-
+from keras.layers import Dense,Dropout
+from keras.wrappers.scikit_learn import KerasRegressor
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import KFold
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.layers import Dense,Dropout,LSTM,BatchNormalization
+import numpy as np
+import matplotlib.pyplot as plt
 from sklearn import preprocessing
+import random
+from collections import deque
+from sklearn.model_selection import train_test_split
+
+FUTURE_PERIOD_PREDICT = 10
 
 
-mnist = tf.keras.datasets.mnist
-(x_train,y_train), (x_test, y_test) = mnist.load_data()
 
-#normalize data
-x_train = x_train/255.0
-x_test_ = x_test/255.0
+data = pandas.read_csv(r"C:\Users\wietz\Desktop\IoT\ExpID_1_df_125_15min_originalRTT.csv") 
+
+#Preporcess
+
+def NMAE(y,yHat):
+    return np.sum(np.absolute(y - yHat)) / np.sum(y)
+
+
+def preprocessDf(df):
+    
+    tmp = df["logTS"].to_numpy()
+    for x in range(len(tmp)):
+        tmp[x] = tmp[x][0:2]
+        
+    df["hour"] = tmp
+    
+        
+    #TODO should be two classes, weekend or weekday
+    tmp = df["logDate"].to_numpy()
+    for x in range(len(tmp)):
+        tmp[x] = tmp[x][8:10]    
+    
+    df["day"] = tmp
+    
+    #columns that are not necessary should be dropped.
+    df = df.drop(columns=['srcIP', 'Loss','Timestamp', 'DateTime','logDate','logTS',"date","time","RTT"])
+    
+    y = df.pop("future")
+
+
+#    #normalize data
+#    for col in df.columns:
+#        #normalzie everything except target?
+#        if col != "target":
+#            #df[col] = preprocessing.scale(df[col].values)
+            
+    # in case scale generates na
+    df.dropna(inplace=True)
+    
+    sequential_data =[]
+     
+    #deque will pop values when it reaches nFeatures
+    prevTimeSteps = deque(maxlen=FUTURE_PERIOD_PREDICT)
+    
+    for i in df.values:
+        #-1 is for not taking target
+        prevTimeSteps.append([float(n) for n in i[:]])
+        if len(prevTimeSteps) == FUTURE_PERIOD_PREDICT:
+            sequential_data.append(np.array(prevTimeSteps))
+            
+    #random.shuffle(sequential_data)
+    
+    #the data should be balanced.
+    
+    
+    x = []
+    
+    
+    for seq in sequential_data:
+        x.append(seq)
+        
+    return np.array(x),y
+
+
+#create new column, might need old one later? 
+data['future'] = data[f"RTT"].shift(-FUTURE_PERIOD_PREDICT)
+#to make it faster
+data = data[:int(len(data)*0.05):]
+
+x, y = preprocessDf(data)
+
+x_train, x_test,y_train,y_test = train_test_split(x,y,test_size = 0.2, random_state = 4)
+
+
 
 model = Sequential()
-
-#if going to another reccurent layer put : return_sequences=True
-#if going to dense then it wouldnt understadn what a sequence is.
-model.add(LSTM(128,input_shape=(28,28), activation="relu", return_sequences=True ))
+model.add(LSTM(128,input_shape=(train_x.shape[1:]),activation="tanh",return_sequences=True))
 model.add(Dropout(0.2))
+model.add(BatchNormalization())
 
-model.add(LSTM(128,activation="relu"))
+model.add(LSTM(128,input_shape=(train_x.shape[1:]),activation="tanh",return_sequences=True))
+model.add(Dropout(0.1))
+model.add(BatchNormalization())
+
+model.add(LSTM(128,activation="relu",input_shape=(train_x.shape[1:])))
 model.add(Dropout(0.2))
+model.add(BatchNormalization())
 
-#its normal to have one dense layer before going to output.
 model.add(Dense(32,activation="relu"))
 model.add(Dropout(0.2))
-
-
-#for this we have 10 classes
-model.add(Dense(10,activation="softmax"))
-
-#decay will decrease lr. So it will stop bouncing around alot, You want to only take bigger learning steps in the beginning
-opt= adam(lr=1e-3, decay = 1e-5)
-
-#TODO look this up
-model.compile(loss="sparse_categorical_crossentropy",
-              optimizer=opt,
-              metrics=['accuracy'])
-
-model.fit(x_train,y_train, epochs=3, validation_data = (x_test,y_test))

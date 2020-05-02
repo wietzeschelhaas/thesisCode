8c72c91f08725eb221bca9cdbac3546c4ea90a3c
wietzeschelhaas <wietze.pc@gmail.com>
2020-05-02 15:56:54 +0200

    working lstm on iot data

--- lstm.py
+++ lstm.py
@@ -5,23 +5,17 @@ Created on Wed Apr 22 09:30:37 2020
 @author: wietz
 """
 
-import pandas
-from keras.models import Sequential
-from keras.layers import Dense,Dropout
-from keras.wrappers.scikit_learn import KerasRegressor
-from sklearn.model_selection import cross_val_score
-from sklearn.model_selection import KFold
-from sklearn.model_selection import train_test_split
-from tensorflow.keras.layers import Dense,Dropout,LSTM,BatchNormalization
-import numpy as np
-import matplotlib.pyplot as plt
+import pandas as pd
 from sklearn import preprocessing
-import random
 from collections import deque
-from sklearn.model_selection import train_test_split
+import random
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense,Dropout,LSTM,BatchNormalization
+from tensorflow.keras.callbacks import TensorBoard, ModelCheckpoint
 
-FUTURE_PERIOD_PREDICT = 10
-
+FUTURE_PERIOD_PREDICT = 5
 
 
 data = pandas.read_csv(r"C:\Users\wietz\Desktop\IoT\ExpID_1_df_125_15min_originalRTT.csv") 
@@ -52,6 +46,7 @@ def preprocessDf(df):
     df = df.drop(columns=['srcIP', 'Loss','Timestamp', 'DateTime','logDate','logTS',"date","time","RTT"])
     
     y = df.pop("future")
+    y.dropna(inplace=True)
 
 
 #    #normalize data
@@ -61,14 +56,14 @@ def preprocessDf(df):
 #            #df[col] = preprocessing.scale(df[col].values)
             
     # in case scale generates na
-    df.dropna(inplace=True)
+    #df.dropna(inplace=True)
     
     sequential_data =[]
      
     #deque will pop values when it reaches nFeatures
     prevTimeSteps = deque(maxlen=FUTURE_PERIOD_PREDICT)
     
-    for i in df.values:
+    for i in df.values[:-1]:
         #-1 is for not taking target
         prevTimeSteps.append([float(n) for n in i[:]])
         if len(prevTimeSteps) == FUTURE_PERIOD_PREDICT:
@@ -90,27 +85,34 @@ def preprocessDf(df):
 
 #create new column, might need old one later? 
 data['future'] = data[f"RTT"].shift(-FUTURE_PERIOD_PREDICT)
+
 #to make it faster
-data = data[:int(len(data)*0.05):]
+#data = data[:int(len(data)*0.05):]
 
 x, y = preprocessDf(data)
 
-x_train, x_test,y_train,y_test = train_test_split(x,y,test_size = 0.2, random_state = 4)
+msk = np.random.rand(len(x)) < 0.8
+x_train = x[msk]
+x_test = x[~msk]
+
+y_train = y[msk]
+y_test = y[~msk]
+
+#x_train, x_test,y_train,y_test = train_test_split(x,y,test_size = 0.2, random_state = 4)
 
 
 
 model = Sequential()
-model.add(LSTM(128,input_shape=(train_x.shape[1:]),activation="tanh",return_sequences=True))
-model.add(Dropout(0.2))
-model.add(BatchNormalization())
+model.add(LSTM(50, input_shape=(x_train.shape[1], x_train.shape[2])))
+model.add(Dense(1))
+model.compile(loss='mae', optimizer='adam')
 
-model.add(LSTM(128,input_shape=(train_x.shape[1:]),activation="tanh",return_sequences=True))
-model.add(Dropout(0.1))
-model.add(BatchNormalization())
 
-model.add(LSTM(128,activation="relu",input_shape=(train_x.shape[1:])))
-model.add(Dropout(0.2))
-model.add(BatchNormalization())
+history = model.fit(x_train, y_train, epochs=50, batch_size=72, validation_data=(x_test, y_test), verbose=1, shuffle=False)
 
-model.add(Dense(32,activation="relu"))
-model.add(Dropout(0.2))
+
+# plot history
+pyplot.plot(history.history['loss'], label='train')
+pyplot.plot(history.history['val_loss'], label='test')
+pyplot.legend()
+pyplot.show()
